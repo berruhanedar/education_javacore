c06a2b07619e99a7f16403998368474de1eed222
Berru Hanedar <berruhanedar@icloud.com>
2025-02-07 02:24:51 +0300

    class, psvm, sout

--- src/main/java/com/berruhanedar/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/berruhanedar/_1_part_javatypes/_01_JavaTypes.java
@@ -1,9 +1,14 @@
 package com.berruhanedar._1_part_javatypes;
 
+// Single Comment
+/*
+Multiple Comment
+ */
 public class _01_JavaTypes {
 
-    //psvm
+    //psvm => tab
     public static void main(String[] args) {
-
-    }
-}
+        //sout => tab
+        System.out.println("Berru Hanedar");
+    } //end psvm
+} //end class
