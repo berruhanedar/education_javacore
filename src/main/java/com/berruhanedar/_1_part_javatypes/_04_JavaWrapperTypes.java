ba23aa7baa4d9655c17de2140aeff54432079c4f
Berru Hanedar <berruhanedar@icloud.com>
2025-02-07 04:19:32 +0300

    update

--- src/main/java/com/berruhanedar/_1_part_javatypes/_04_JavaWrapperTypes.java
+++ src/main/java/com/berruhanedar/_1_part_javatypes/_04_JavaWrapperTypes.java
@@ -2,27 +2,7 @@ package com.berruhanedar._1_part_javatypes;
 
 public class _04_JavaWrapperTypes {
     public static void main(String[] args) {
-        //operator
-        //+, -, *, /
-        // %
-        // ++
-        // --
 
-        int number1 = 23, number2 = 7;
-        int addition = number1 + number2;
-        int subtraction = number1 - number2;
-        int multiplication = number1 * number2;
-        int division = number1 / number2;
-        int modulus = (number1 % number2);
-
-        System.out.println("Addition: " + addition);
-        System.out.println("Subtraction: " + subtraction);
-        System.out.println("Multiplication: " + multiplication);
-        System.out.println("Division: " + division);
-        System.out.println("Modulus: " + modulus);
-
-        // LOGICAL: && = AND || = OR != NOT
-        // ++1 INCREMENT --1 DECREMENT
 
     }
 }
