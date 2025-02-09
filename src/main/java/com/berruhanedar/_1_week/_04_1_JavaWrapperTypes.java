452909c0e21d5fcf2c7c1546dd4d9a680e1a7a72
Berru Hanedar <berruhanedar@icloud.com>
2025-02-09 03:43:59 +0300

    improve code formatting in _04_1_JavaWrapperTypes

--- src/main/java/com/berruhanedar/_1_week/_04_1_JavaWrapperTypes.java
+++ src/main/java/com/berruhanedar/_1_week/_04_1_JavaWrapperTypes.java
@@ -3,35 +3,34 @@ package com.berruhanedar._1_week;
 public class _04_1_JavaWrapperTypes {
     public static void main(String[] args) {
         //PRIMITIVE TYPE
-        byte primitiveTypeByte=127;
+        byte primitiveTypeByte = 127;
         // primitive typelar can not be null ==  primitiveTypeByte=null;
         System.out.println(primitiveTypeByte);
-        short primitiveTypeShort=5699;
-        int primitiveTypeInt=49846846;
-        long primitiveTypeLong=4294967295L;
+        short primitiveTypeShort = 5699;
+        int primitiveTypeInt = 49846846;
+        long primitiveTypeLong = 4294967295L;
 
-        float primitiveTypeFloat=429496725795.0f;
-        double primitiveTypeDouble=65868566816186295d;
+        float primitiveTypeFloat = 429496725795.0f;
+        double primitiveTypeDouble = 65868566816186295d;
 
-        boolean primitiveTypeBoolean=true;
-        char primitiveTypeChar='a';
+        boolean primitiveTypeBoolean = true;
+        char primitiveTypeChar = 'a';
 
         ///////////////////////////////////////////////////////////////////////////////////////
 
         //WRAPPER TYPE
-        Byte wrapperTypeByte=127;
+        Byte wrapperTypeByte = 127;
         // wrapper types can be null
-        wrapperTypeByte=null;
+        wrapperTypeByte = null;
         System.out.println(wrapperTypeByte);
-        Short wrapperTypeShort=5699;
-        Integer wrapperTypeInt=49846846;
-        Long wrapperTypeLong=4294967295L;
+        Short wrapperTypeShort = 5699;
+        Integer wrapperTypeInt = 49846846;
+        Long wrapperTypeLong = 4294967295L;
 
-        Float wrapperTypeFloat=429496725795.0f;
-        Double wrapperTypeDouble=65868566816186295d;
+        Float wrapperTypeFloat = 429496725795.0f;
+        Double wrapperTypeDouble = 65868566816186295d;
 
-        Boolean wrapperTypeBoolean=true;
-        Character wrapperTypeChar='a';
-
+        Boolean wrapperTypeBoolean = true;
+        Character wrapperTypeChar = 'a';
     }
 }
