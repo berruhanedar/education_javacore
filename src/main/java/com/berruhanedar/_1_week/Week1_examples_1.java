6efd77ecaba5ee5ebfb1a9180d662887d5358371
Berru Hanedar <berruhanedar@icloud.com>
2025-02-09 07:38:22 +0300

    first-degree equation solver in Week1_examples_1

---
+++ src/main/java/com/berruhanedar/_1_week/Week1_examples_1.java
@@ -0,0 +1,33 @@
+package com.berruhanedar._1_week;
+
+import java.util.Scanner;
+
+public class Week1_examples_1 {
+    public static void main(String[] args) {
+        /*
+       1. Solving a First-Degree Equation with One Unknown (Using Scanner)
+       Problem:
+       Write a program that takes the values of 'a' and 'b' from the user in the equation ax + b = 0
+       and calculates the value of 'x'.
+       Solution:
+        */
+
+        //Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Variable
+        double aValue, bValue, result;
+
+        System.out.println("Enter a:");
+        aValue = sc.nextDouble();
+
+        System.out.println("Enter b:");
+        bValue = sc.nextDouble();
+
+        //ax+b =0
+        result = (-bValue) / aValue;
+        System.out.println("a value:" + aValue + " b value:" +bValue+ " The result is:" + result);
+
+        sc.close();
+    }
+}
