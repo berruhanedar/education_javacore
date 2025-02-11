ad594a39240eac355cb3abff3150cc4a508578b0
Berru Hanedar <berruhanedar@icloud.com>
2025-02-12 01:16:03 +0300

    added conditional statements in Java

---
+++ src/main/java/com/berruhanedar/_2_week/_09_1_Conditional.java
@@ -0,0 +1,13 @@
+package com.berruhanedar._2_week;
+
+public class _09_1_Conditional {
+    public static void main(String[] args) {
+        int number = 5;
+        if (number > 0) {
+            System.out.println("It's positive number");
+        } else {
+            System.out.println("It's negative number");
+        }
+
+    }
+}
