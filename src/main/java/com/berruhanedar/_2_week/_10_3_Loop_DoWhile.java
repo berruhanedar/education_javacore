4eba0ba1400cfed7bf84590267a043860da0093b
Berru Hanedar <berruhanedar@icloud.com>
2025-02-12 01:49:56 +0300

    implement for loop - do while structure

---
+++ src/main/java/com/berruhanedar/_2_week/_10_3_Loop_DoWhile.java
@@ -0,0 +1,14 @@
+package com.berruhanedar._2_week;
+
+public class _10_3_Loop_DoWhile {
+    public static void main(String[] args) {
+        // Regardless of whether the condition is met or not, it will execute at least once.
+        int i = 5;
+        do {
+            System.out.print(i + " - ");
+            i++;
+
+        } while (i <= 10);
+
+    }
+}
