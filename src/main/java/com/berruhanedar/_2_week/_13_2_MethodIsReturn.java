3c9b1735ab7a209831803fdce44463bf7973a2bc
Berru Hanedar <berruhanedar@icloud.com>
2025-02-14 08:07:04 +0300

    methods with return values and parameters

---
+++ src/main/java/com/berruhanedar/_2_week/_13_2_MethodIsReturn.java
@@ -0,0 +1,26 @@
+package com.berruhanedar._2_week;
+
+public class _13_2_MethodIsReturn {
+
+    // 3 - With return without parameters
+    public String methodWithReturnWithoutParameters(){
+        return "methodWithReturnWithoutParameters";
+    }
+
+    // 4 - With return and parameters
+    public Integer methodWithReturAndParameters(int number){
+        return number;
+    }
+
+    public static void main(String[] args) {
+        _13_2_MethodIsReturn method = new _13_2_MethodIsReturn();
+        String result = method.methodWithReturnWithoutParameters();
+        System.out.println(result);
+
+        Integer result2 = method.methodWithReturAndParameters(1);
+        System.out.println(result2);
+
+    }
+
+
+}
