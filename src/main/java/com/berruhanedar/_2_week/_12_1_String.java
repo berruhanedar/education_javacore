2015733c20c0f35758af66aaf5a8f4f43bbca259
Berru Hanedar <berruhanedar@icloud.com>
2025-02-14 07:39:54 +0300

    add String operations and enhancements

--- src/main/java/com/berruhanedar/_2_week/_12_1_String.java
+++ src/main/java/com/berruhanedar/_2_week/_12_1_String.java
@@ -45,10 +45,48 @@ public class _12_1_String {
         // System.out.println(vocabulary1.trim().substring(1, 40)); // StringIndexOutOfBoundsException
 
         String vocabulary3 = "Java";
+        // StringIndexOutOfBoundsException
         if (vocabulary3.length() >= 10)
             System.out.println(vocabulary3.substring(0, 10));
         else
             System.out.println("The word that you have given has " + vocabulary3.length() + " characters."); // Display the length if it's less than 10
+
+        //Contains
+        System.out.println(vocabulary1.contains("Java"));
+
+        //Split
+
+
+        //Replace
+        System.out.println("Change:" + vocabulary1.replace('a', 'e'));
+        System.out.println(vocabulary1.replace("Java", "English"));
+        System.out.println(vocabulary1);
+        vocabulary1 = vocabulary1.replace('a', 'e');
+        System.out.println(vocabulary1);
+
+        //Concat
+        System.out.println(vocabulary1.concat("well").concat("do"));
+        String concatenation = vocabulary1 + vocabulary2 + vocabulary3;
+
+        //StringBuilder
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(vocabulary1).append("well").append("do");
+        String dataToString1 = stringBuilder.toString();
+        System.out.println(dataToString1);
+
+        //StringBuffer
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(vocabulary1).append("well").append("do");
+        String dataToString2 = stringBuffer.toString();
+        System.out.println(dataToString2);
+
+        vocabulary1.hashCode();
+        System.out.println(vocabulary1.hashCode());
+
+
+
+
+
     }
 }
 
