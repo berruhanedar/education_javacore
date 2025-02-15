9be7e70c1cb1395c9acab88b4a67082c9cc64cde
Berru Hanedar <berruhanedar@icloud.com>
2025-02-15 15:52:35 +0300

    add methods to display current date and time in different formats

---
+++ src/main/java/com/berruhanedar/_2_week/_15_1_Date.java
@@ -0,0 +1,56 @@
+package com.berruhanedar._2_week;
+
+import java.util.Date;
+
+public class _15_1_Date {
+
+    // Date
+    public static void dateMethod() {
+        Date date = new Date();
+
+        System.out.println("Current time: " + date);
+        System.out.println("Milliseconds since January 1, 1970 until now: " + date.getTime());
+        System.out.println("Date: " + date.getDate());
+
+        System.out.println("#######################");
+        System.out.println("Day: " + date.getDay());
+        System.out.println("Month: " + date.getMonth()); // Starts counting from 0 = January, 1 = February
+        System.out.println("Year: " + (1900 + date.getYear()));  // Add or subtract 1900
+        System.out.println("Years passed since: " + (2025 - date.getYear()));
+        System.out.println("Hours: " + date.getHours());
+        System.out.println("Minutes: " + date.getMinutes());
+        System.out.println("Seconds: " + date.getSeconds());
+    }
+
+    public static String nowFormat1() throws NullPointerException {
+        Date now = new Date();
+        String specialFormat = "Time: "
+                .concat(String.valueOf(now.getHours()))
+                .concat(":")
+                .concat(String.valueOf(now.getMinutes()))
+                .concat(":")
+                .concat(String.valueOf(now.getSeconds()))
+                .toString();
+        return specialFormat;
+    }
+
+    public static String nowFormat2() throws NullPointerException {
+        Date now = new Date();
+        // %s : String
+        // %d : Decimal
+        // %f : Float
+        return String.format("Current time: %02d:%02d:%02d", now.getHours(), now.getMinutes(), now.getSeconds());
+    }
+
+    // PSVM
+    public static void main(String[] args) {
+        //dateMethod();
+
+        /*String nowFormat1 = nowFormat1();
+        System.out.println(nowFormat1);*/
+
+        String nowDate2 = nowFormat2();
+        System.out.println(nowDate2);
+
+    }
+}
