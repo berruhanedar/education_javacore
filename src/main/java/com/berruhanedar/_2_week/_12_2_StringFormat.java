1e67f64b0b644fb940d04f9f74199cfe239e7e99
Berru Hanedar <berruhanedar@icloud.com>
2025-02-15 17:59:19 +0300

    refactor string formatting with Formatter and String.format

--- src/main/java/com/berruhanedar/_2_week/_12_2_StringFormat.java
+++ src/main/java/com/berruhanedar/_2_week/_12_2_StringFormat.java
@@ -4,6 +4,14 @@ import java.util.Formatter;
 
 public class _12_2_StringFormat {
 
+     /*
+        Garbage Collectors:
+        Garbage collectors only clean up objects that are not associated with open resources
+        like files, scanners, formatters, etc. If these resources are not manually closed,
+        the garbage collector can clean up the unused object, but it won't close the open file.
+        If the file remains open, the cache memory will continue to be used.
+        */
+
     // Formatter
     // s
     // d
@@ -14,18 +22,18 @@ public class _12_2_StringFormat {
         formatter.format("Hello , Your Name : %s , Your Number : %d , Price : %f", " John Doe", 123, 58.54);
         System.out.println(formatter);
         formatter.close(); // free up memory
-
-        /*
-        Garbage Collectors:
-        Garbage collectors only clean up objects that are not associated with open resources
-        like files, scanners, formatters, etc. If these resources are not manually closed,
-        the garbage collector can clean up the unused object, but it won't close the open file.
-        If the file remains open, the cache memory will continue to be used.
-        */
     }
+
+    // Best practice
+    public static void formatter2() {
+        String formatterString = String.format("Hello , Your Name : %s , Your Number : %d , Price : %f", " John Doe", 123, 58.54);
+        System.out.println(formatterString);
+    }
+
 
     public static void main(String[] args) {
         formatter1();
+        formatter2();
     }
 
 }
