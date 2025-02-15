83fcd1c4896694e94746b9ff4b1462c56a8dec6b
Berru Hanedar <berruhanedar@icloud.com>
2025-02-15 18:38:32 +0300

    enhance array manipulation examples

--- src/main/java/com/berruhanedar/_2_week/_16_1_Array.java
+++ src/main/java/com/berruhanedar/_2_week/_16_1_Array.java
@@ -1,5 +1,7 @@
 package com.berruhanedar._2_week;
 
+import java.util.Arrays;
+
 public class _16_1_Array {
 
     public static String[] arrayMethod1() throws ArrayIndexOutOfBoundsException {
@@ -31,10 +33,31 @@ public class _16_1_Array {
         }
     }
 
+    // iterative
+    public static void arrayMethod4() {
+        String[] city = arrayMethod2();
+        // for each
+        for (int i = 0; i < city.length; i++) {
+            System.out.println(_15_4_SpecialColor.BLUE + city[i] + _15_4_SpecialColor.RESET);
+        }
+    }
+
+    // Foreach
+    public static void arrayMethod5() {
+        String[] city = arrayMethod2();
+        Arrays.sort(city);
+
+        for (String temp : city) {
+            System.out.println(_15_4_SpecialColor.YELLOW + temp + _15_4_SpecialColor.RESET);
+        }
+    }
+
     public static void main(String[] args) {
         arrayMethod1();
         arrayMethod2();
         arrayMethod3();
+        arrayMethod4();
+        arrayMethod5();
     }
 
 }
