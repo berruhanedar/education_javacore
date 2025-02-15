40b22ed99770ff0a507fc75eb769d2f221638212
Berru Hanedar <berruhanedar@icloud.com>
2025-02-15 16:17:09 +0300

    update LocalDateTime formatting to support Turkish locale

--- src/main/java/com/berruhanedar/_2_week/_15_2_LocalDateTime.java
+++ src/main/java/com/berruhanedar/_2_week/_15_2_LocalDateTime.java
@@ -2,19 +2,21 @@ package com.berruhanedar._2_week;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class _15_2_LocalDateTime {
-    public static void LocalDateFormatMethod(){
+    public static void LocalDateFormatMethod() {
         LocalDateTime now = LocalDateTime.now();
+        Locale locale = new Locale("tr", "TR");
         System.out.println("Current Time: " + now);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss", locale);
         System.out.println("Current Time: " + now.format(formatter));
 
     }
 
 
     public static void main(String[] args) {
-        _15_2_LocalDateTime.LocalDateFormatMethod();
+        LocalDateFormatMethod();
     }
 }
