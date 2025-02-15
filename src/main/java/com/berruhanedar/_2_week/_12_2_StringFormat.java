14e077d586ce3771d9ff6e721784fbaf81b3827b
Berru Hanedar <berruhanedar@icloud.com>
2025-02-15 18:13:41 +0300

    refactor string formatting with best practices and additional methods

--- src/main/java/com/berruhanedar/_2_week/_12_2_StringFormat.java
+++ src/main/java/com/berruhanedar/_2_week/_12_2_StringFormat.java
@@ -18,22 +18,43 @@ public class _12_2_StringFormat {
     // f
 
     public static void formatter1() {
+        // When using a new Formatter, it's good practice to call close() to release system resources
         Formatter formatter = new Formatter();
         formatter.format("Hello , Your Name : %s , Your Number : %d , Price : %f", " John Doe", 123, 58.54);
         System.out.println(formatter);
         formatter.close(); // free up memory
     }
 
-    // Best practice
+
     public static void formatter2() {
+
+    }
+
+    // Best practice
+    // We don't have to use close(), as the garbage collector runs automatically.
+    public static void formatter3() {
         String formatterString = String.format("Hello , Your Name : %s , Your Number : %d , Price : %f", " John Doe", 123, 58.54);
         System.out.println(formatterString);
+    }
+
+    // Best Practice
+    public static void formatter4() {
+        String name = "Berru";
+        int idNumber = 56445;
+        double price = 123.45;
+        System.out.printf("Hello,  Your Name : %s , Your Number : %d , Price : %f", name, idNumber, price);
+        System.out.println();
+        System.out.printf("Hello,  Your Name : %10s , Your Number : %d , Price : %f", name, idNumber, price);
+        System.out.println();
+        System.out.printf("Hello,  Your Name : %-10s , Your Number : %-15d , Price : %.3f", name, idNumber, price);
     }
 
 
     public static void main(String[] args) {
         formatter1();
         formatter2();
+        formatter3();
+        formatter4();
     }
 
 }
