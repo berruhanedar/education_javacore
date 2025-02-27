3e63b8cf7d87e2188d48d4de3d95623a7084ce54
berru <berruhanedar@icloud.com>
2025-02-27 22:07:40 +0300

    add default access modifier example"

---
+++ src/main/java/com/berruhanedar/_3_week_access/_02_DefaultAccessModifier.java
@@ -0,0 +1,11 @@
+package com.berruhanedar._3_week_access;
+
+public class _02_DefaultAccessModifier {
+    public static void main(String[] args) {
+        _01_PublicAccessModifier accessModifier = new _01_PublicAccessModifier();
+        System.out.println(accessModifier.publicData);
+        //System.out.println(accessModifier.privateData);
+        System.out.println(accessModifier.protectedData);
+        System.out.println(accessModifier.defaultData);
+    }
+}
