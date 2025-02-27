54de766ebaa2f18d36723af8502f132331e47bf9
berru <berruhanedar@icloud.com>
2025-02-27 22:07:17 +0300

    add public access modifier example"

---
+++ src/main/java/com/berruhanedar/_3_week_access/_01_PublicAccessModifier.java
@@ -0,0 +1,19 @@
+package com.berruhanedar._3_week_access;
+
+public class _01_PublicAccessModifier {
+    public String publicData="public data";
+    private String privateData="private data";
+    protected String protectedData="protected data";
+    String defaultData="public finalData";
+
+
+    public static void main(String[] args) {
+        _01_PublicAccessModifier accessModifier = new _01_PublicAccessModifier();
+        System.out.println(accessModifier.publicData);
+        System.out.println(accessModifier.privateData);
+        System.out.println(accessModifier.protectedData);
+        System.out.println(accessModifier.defaultData);
+    }
+
+
+}
