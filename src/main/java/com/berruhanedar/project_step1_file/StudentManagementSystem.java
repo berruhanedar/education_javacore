66d68c3b233dd55850bc1549b9e786f5416b97a8
berru <berruhanedar@icloud.com>
2025-03-01 01:08:40 +0300

    created StudentManagementSystem class with basic structure and placeholders

---
+++ src/main/java/com/berruhanedar/project_step1_file/StudentManagementSystem.java
@@ -0,0 +1,54 @@
+package com.berruhanedar.project_step1_file;
+
+import java.util.ArrayList;
+
+public class StudentManagementSystem {
+
+    // Field
+    private ArrayList<StudentDto> students = new ArrayList<>();
+    private int studentCounter=0;
+    private static final String FILE_NAME="student.txt";
+
+    // static
+    static{
+    }
+
+    // Constructor without parameter
+    public StudentManagementSystem() {
+        // Load the student list immediately when the program executes
+        loadStudentsListFromFiled();
+    }
+
+    // Upload Student List
+    private void loadStudentsListFromFiled() {
+
+    }
+
+    // Add Students
+
+    // Student List
+
+    // Search Student
+
+    // Update Student
+
+    // Delete Student
+
+    //////////////////////////////
+
+    // File Create - IO
+    // Method That Saves Student Files
+
+    // Öethod That Reads Student Files
+
+    //////////////////////////////
+    // Total Number of Students
+
+    // Calculate Student's Grade Average
+
+    // Student with the Highest or Lowest Grade
+
+    // Student Ranking (Birthdate)
+
+    // Console Chose (Add Student)
+}
