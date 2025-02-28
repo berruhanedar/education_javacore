92689cb6f92c15bbebb46e42bfa2f0d0b2ab9e62
berru <berruhanedar@icloud.com>
2025-03-01 00:47:06 +0300

    added the Student class with Lombok annotations and serialization support.

---
+++ src/main/java/com/berruhanedar/project_step1_file/Student.java
@@ -0,0 +1,97 @@
+package com.berruhanedar.project_step1_file;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Date;
+
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode
+
+//Student
+public class Student implements Serializable {
+    //Serializable
+    public static final Long serialVersionUID = 1L;
+
+    // Field
+    private Integer id;
+    private String name;
+    private String surname;
+    private LocalDate birthDate;
+    private Date createdDate;
+    private Double grade;
+
+    //static (Created only once throughout the object's lifetime)
+    static {
+    }
+
+    // Constructor without parameter
+    public Student() {
+    }
+
+    // Constructor with parameter
+    public Student(Integer id, String name, String surname, LocalDate birthDate, Double grade) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.grade = grade;
+    }
+
+    // Methods
+
+    // Getter ad Setter
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
+}
