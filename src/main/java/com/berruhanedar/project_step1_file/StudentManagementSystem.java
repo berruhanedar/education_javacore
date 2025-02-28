858fc859b1d19b0e16c12086ae927add76ef027f
berru <berruhanedar@icloud.com>
2025-03-01 01:59:06 +0300

    added file operations and student management functionalities to StudentManagementSystem

--- src/main/java/com/berruhanedar/project_step1_file/StudentManagementSystem.java
+++ src/main/java/com/berruhanedar/project_step1_file/StudentManagementSystem.java
@@ -1,16 +1,20 @@
 package com.berruhanedar.project_step1_file;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class StudentManagementSystem {
 
     // Field
-    private ArrayList<StudentDto> students = new ArrayList<>();
-    private int studentCounter=0;
-    private static final String FILE_NAME="student.txt";
+    private ArrayList<StudentDto> studentDtoList = new ArrayList<>();
+    private int studentCounter = 0;
+    private static final String FILE_NAME = "student.txt";
 
     // static
-    static{
+    static {
     }
 
     // Constructor without parameter
@@ -28,19 +32,74 @@ public class StudentManagementSystem {
     // Login
     // Register
     //////////////////////////////////////////////////
-    // Add Students
-    // Student List
-    // Search Student
-    // Update Student
-    // Delete Student
-    //////////////////////////////////////////////////
     // File Create - IO
-    // Method That Saves Student Files
-    // Öethod That Reads Student Files
+    // File Create
+    private void saveToFile() {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            objectOutputStream.writeObject(studentDtoList);
+        } catch (IOException io) {
+            System.out.println("File write error");
+            io.printStackTrace();
+        }
+    }
+
+    // File Read
+    // Upload Student List (File)
     //////////////////////////////
+    // Add Students
+    public void add(StudentDto studentDto) {
+        studentDtoList.add(new StudentDto(++studentCounter, studentDto.getName(), studentDto.getSurname(), studentDto.getBirthDate(), studentDto.getGrade()));
+        System.out.println("Added student");
+        // Add to File
+        saveToFile();
+    }
+
+    // Student List
+    public void list() {
+        if (studentDtoList.isEmpty()) {
+            System.out.println("List is empty");
+            return;
+        } else {
+            studentDtoList.forEach(System.out::println);
+        }
+    }
+
+    // Search Student
+    public void search(String name) {
+        studentDtoList.stream()
+                .filter(temp -> temp.getName().equalsIgnoreCase(name))
+                .forEach(System.out::println);
+    }
+
+    // Update Student
+    public void update(int id, StudentDto dto){
+        for(StudentDto temp : studentDtoList){
+            if(temp.getId() == id){
+                temp.setName(dto.getName());
+                temp.setSurname(dto.getSurname());
+                temp.setBirthDate(dto.getBirthDate());
+                temp.setGrade(dto.getGrade());
+                System.out.println("Student updated");
+                saveToFile();
+                return;
+            }
+        }
+        System.out.println("Student not found");
+    }
+
+    // Delete Student
+    public void delete(int id) {
+        studentDtoList.removeIf(temp -> temp.getId() == id);
+        System.out.println("Student deleted");
+    }
+
+    //////////////////////////////////////////////////
+
     // Total Number of Students
     // Calculate Student's Grade Average
     // Student with the Highest or Lowest Grade
     // Student Ranking (Birthdate)
     // Console Chose (Add Student)
+
+
 }
