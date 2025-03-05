ac36666f211d6cf5016a0c27357a8765b8f1a519
berru <berruhanedar@icloud.com>
2025-03-05 19:31:40 +0300

    CRUD

---
+++ src/main/java/com/berruhanedar/MainTest.java
@@ -0,0 +1,11 @@
+package com.berruhanedar;
+
+import com.berruhanedar.project_step1_file.StudentManagementSystem;
+
+public class MainTest {
+    public static void main(String[] args) {
+        StudentManagementSystem studentManagementSystem = new StudentManagementSystem();
+        studentManagementSystem.chooise();
+    }
+}
+
