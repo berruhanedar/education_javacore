e0040948763de5ad75b9452b6133d9f1e6236f92
berru <berruhanedar@icloud.com>
2025-03-06 03:13:48 +0300

    project in files migrated

--- src/main/java/com/berruhanedar/MainTest.java
+++ src/main/java/com/berruhanedar/MainTest.java
@@ -1,11 +1,11 @@
 package com.berruhanedar;
 
-import com.berruhanedar.project_step1_file.StudentManagementSystem;
+import com.berruhanedar.dao.StudentDao;
 
 public class MainTest {
     public static void main(String[] args) {
-        StudentManagementSystem studentManagementSystem = new StudentManagementSystem();
-        studentManagementSystem.chooise();
+        StudentDao studentDao = new StudentDao();
+        studentDao.chooise();
     }
 }
 
