7ee87c20daa179e7f177a1bce1c76cc794eb361d
berru <berruhanedar@icloud.com>
2025-03-06 04:01:41 +0300

    refactor MainTest to use StudentController

--- src/main/java/com/berruhanedar/MainTest.java
+++ src/main/java/com/berruhanedar/MainTest.java
@@ -1,11 +1,12 @@
 package com.berruhanedar;
 
+import com.berruhanedar.controller.StudentController;
 import com.berruhanedar.dao.StudentDao;
 
 public class MainTest {
     public static void main(String[] args) {
-        StudentDao studentDao = new StudentDao();
-        studentDao.chooise();
+        StudentController studentController  = new StudentController();
+        studentController.chooise();
     }
 }
 
