f946acde99dc3855feca3c32d778744d5f6934a9
berru <berruhanedar@icloud.com>
2025-03-06 04:00:56 +0300

    add StudentController class with CRUD operations

--- src/main/java/com/berruhanedar/controller/StudentController.java
+++ src/main/java/com/berruhanedar/controller/StudentController.java
@@ -1,4 +1,50 @@
 package com.berruhanedar.controller;
 
-public class StudentController {
+import com.berruhanedar.dao.IDaoGenerics;
+import com.berruhanedar.dao.StudentDao;
+import com.berruhanedar.dto.StudentDto;
+
+import java.util.ArrayList;
+
+public class StudentController implements IDaoGenerics<StudentDto> {
+
+    // Injection
+    private StudentDao studentDao;
+
+    // Constructor without parameter
+    public StudentController() {
+        studentDao = new StudentDao();
+    }
+
+    ////////////////////////////
+    // CRUD
+    @Override
+    public StudentDto create(StudentDto studentDto) {
+        return studentDao.create(studentDto);
+    }
+
+    @Override
+    public StudentDto findByName(String name) {
+        return studentDao.findByName(name);
+    }
+
+    @Override
+    public ArrayList<StudentDto> list() {
+        return studentDao.list();
+    }
+
+    @Override
+    public StudentDto update(int id, StudentDto studentDto) {
+        return studentDao.update(id, studentDto);
+    }
+
+    @Override
+    public StudentDto delete(int id) {
+        return studentDao.delete(id);
+    }
+
+    @Override
+    public void chooise(){
+        studentDao.chooise();
+    }
 }
