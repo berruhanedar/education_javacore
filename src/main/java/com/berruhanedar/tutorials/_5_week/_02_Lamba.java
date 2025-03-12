0b91186525da70535e5c9c1779c4ac985fbc9574
berru <berruhanedar@icloud.com>
2025-03-12 13:17:56 +0300

    updated

--- src/main/java/com/berruhanedar/tutorials/_5_week/_02_Lamba.java
+++ src/main/java/com/berruhanedar/tutorials/_5_week/_02_Lamba.java
@@ -1,6 +1,6 @@
 package com.berruhanedar.tutorials._5_week;
 
-
+// Java 8 introduced @FunctionalInterface annotation (to define an interface with exactly one abstract method):@FunctionalInterface
 @FunctionalInterface
 interface MyFunctionalInterface{
     void showMessage(String message);
@@ -11,5 +11,4 @@ public class _02_Lamba {
         MyFunctionalInterface messagePrinter = (temp)-> System.out.println("Message: " + temp);
         messagePrinter.showMessage("Hello Lambda!");
     }
-
 }
